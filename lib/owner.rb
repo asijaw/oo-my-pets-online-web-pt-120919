7eff0b0e44c2ed5b3635ce253935eb58093d7dc2
Asija Watson <asija.w@yahoo.com>
2020-01-19 03:01:35 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,24 @@
 class Owner
   attr_reader :name, :species
+  
+  @@all = []
 
 
   def initialize(name)
     @name = name
     @species = "human"
+    save
+  end
+  
+  def save
+    @@all << self
   end
   
   def say_species
     "I am a #{species}."
+  end
+  
+  def self.all
+    @@all
   end
 end 
