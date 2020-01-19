c6da1047b3f976c6cc1c5a41f0a0507840fb9131
Asija Watson <asija.w@yahoo.com>
2020-01-19 02:46:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,12 @@
 class Owner
   attr_reader :name
-  attr_accessor :species
+  @@species
 
   def initialize(name)
     @name = name
   end
   
   def species
-    @species = human
+    @@species = human
   end
 end 
