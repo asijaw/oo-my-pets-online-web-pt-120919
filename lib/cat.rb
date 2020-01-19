32a77e24a57b3b9eb2c9a05518298235af26eb4b
Asija Watson <asija.w@yahoo.com>
2020-01-19 03:06:33 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,16 @@
 class Cat
   attr_reader :name 
   attr_accessor :owner, :mood
+  @@cats = []
+  
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    save
   end
+  
+  def save
+    @@cats << self
+  end 
 end
